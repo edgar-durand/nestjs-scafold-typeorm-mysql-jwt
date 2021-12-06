89266617c4a74483f1de980243970995b7ed7ab3
unknown <edgar.durand@ntsprint.com>
2021-12-06 16:13:04 -0500

    init

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+
   const options = new DocumentBuilder()
     .setTitle('We Propel API')
     .setDescription('') //TODO: DEFINE API DESCRIPTION
